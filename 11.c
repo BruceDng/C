c6742bbfbc22a1cae790e36dfb495eb36888f0df
Spark <2071049449@qq.com>
2022-04-01 22:28:35 +0800

    ......

---
+++ 11.c
@@ -0,0 +1,11 @@
+//用*号输出字母C的图案。
+#include <stdio.h>
+
+int main(){
+	printf("用 * 号输出字母 C!\n");
+    printf(" ****\n");
+    printf(" *\n");
+    printf(" * \n");
+    printf(" ****\n");
+	return 0;
+}
